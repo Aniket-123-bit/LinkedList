a9bd3ac7c99f2938cc00cab84712cc2b26e18ced
Aniket-123-bit <anisunny782@gmail.com>
2021-06-23 11:54:58 +0530

    Commit Uc3 Push

--- src/main/java/com/linkedlist/LinkedList.java
+++ src/main/java/com/linkedlist/LinkedList.java
@@ -3,17 +3,19 @@ package com.linkedlist;
 public class LinkedList {
     public static void main(String[] args) {
         LinkedListOperations operations = new LinkedListOperations();
-        INode head;
-        Node<Integer> myFirstNode = new Node<>(70);
-        Node<Integer> mySecondNode = new Node<>(30);
-        Node<Integer> myThirdNode = new Node<>(56);
 
-        head = operations.addNode(myFirstNode);
-        operations.displayNode(head);
-        head = operations.addNode(mySecondNode);
-        operations.displayNode(head);
-        head = operations.addNode(myThirdNode);
-        operations.displayNode(head);
+        Node<Integer> myFirstNode = new Node<>(56);
+        Node<Integer> mySecondNode = new Node<>(30);
+        Node<Integer> myThirdNode = new Node<>(70);
+
+         operations.addNode(myFirstNode);
+
+         operations.addNode(mySecondNode);
+
+         operations.addNode(myThirdNode);
+
+        operations.displayNode();
+
     }
 }
 
